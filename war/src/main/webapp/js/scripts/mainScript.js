2565d8dd2063cf99491b2a9075b398f0e0164df0
vineetind <jain.vineet@hotmail.com>
2014-08-04 18:05:45 +0530

    Complete Angular Setup with login

---
+++ war/src/main/webapp/js/scripts/mainScript.js
@@ -0,0 +1,72 @@
+	// create the module and name it scotchApp
+	var scotchApp = angular.module('scotchApp', ['ngRoute']);
+
+	// configure our routes
+	scotchApp.config(function($routeProvider) {
+		$routeProvider
+
+			// route for the home page
+			.when('/', {
+				templateUrl : 'pages/home.html',
+				controller  : 'mainController'
+			})
+
+			// route for the about page
+			.when('/about', {
+				templateUrl : 'pages/about.html',
+				controller  : 'aboutController'
+			})
+
+			// route for the contact page
+			.when('/contact', {
+				templateUrl : 'pages/contact.html',
+				controller  : 'contactController'
+			})
+			//rout for the login page
+			.when('/login', {
+			templateUrl : 'pages/login.html',
+			controller  : 'loginController'
+		});
+	});
+
+	// create the controller and inject Angular's $scope
+	scotchApp.controller('mainController', function($scope) {
+		// create a message to display in our view
+		$scope.message = 'Everyone come and see how good I look!';
+	});
+
+	scotchApp.controller('aboutController', function($scope) {
+		$scope.message = 'Look! I am an about page.';
+	});
+
+	scotchApp.controller('contactController', function($scope) {
+		$scope.message = 'Contact us! JK. This is just a demo.';
+	});
+	
+	scotchApp.controller('loginController', function($scope,$http) {
+		$scope.formData = {};
+
+		// process the form
+		$scope.processForm = function() {
+		$http({
+		method : 'POST',
+		url : 'webservice/Login.action',
+		data : $.param($scope.formData), // pass in data as strings
+		headers : { 'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8' }
+		})
+		.success(function(data) {
+		console.log(data);
+
+		if (!data.jsonMap.success) {
+		// if not successful, bind errors to error variables
+		$scope.errorName = data.jsonMap.errorName;
+		$scope.messages = "";
+		} else {
+		// if successful, bind success message to message
+		$scope.messages = data.jsonMap.messages;
+		$scope.errorName = "";
+		}
+		});
+
+		};
+	});
--- web/src/main/java/com/sample/ui/login/LoginAction.java
+++ web/src/main/java/com/sample/ui/login/LoginAction.java
@@ -1,16 +1,74 @@
 package com.sample.ui.login;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import org.eclipse.jetty.util.ajax.JSONObjectConvertor;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 import com.opensymphony.xwork2.ActionSupport;
 
 public class LoginAction extends ActionSupport {
 	
 	AuthenticationManager authenticationManager; 
+	private String username; 
+	private String password;
+	private Map<String, Object> jsonMap = new LinkedHashMap<String,Object>(); 
 	public String login () 
-	{   
-		
+ {
+
+		if (null != username && null != password) {
+			try {
+				Authentication authentication = authenticationManager
+						.authenticate(new UsernamePasswordAuthenticationToken(
+								username, password));
+				SecurityContextHolder.getContext().setAuthentication(
+						authentication);
+				if (authentication.isAuthenticated()) {
+					jsonMap.put("success", true);
+					jsonMap.put("messages", "Welcome " + username + "!!!!");
+				}
+			} catch (BadCredentialsException be) {
+				jsonMap.put("success", false);
+				jsonMap.put("errorName", "Invalid user name or password !!");
+			}
+
+			// seuritycontexcontextholde
+		} else {
+			jsonMap.put("success", false);
+			jsonMap.put("errorName", "Invalid user name or password !!");
+		}
+
 		return SUCCESS;
+	}
+	
+	public AuthenticationManager getAuthenticationManager() {
+		return authenticationManager;
+	}
+	public void setAuthenticationManager(AuthenticationManager authenticationManager) {
+		this.authenticationManager = authenticationManager;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public Map<String, Object> getJsonMap() {
+		return jsonMap;
+	}
+	public void setJsonMap(Map<String, Object> jsonMap) {
+		this.jsonMap = jsonMap;
 	}
 
 }
