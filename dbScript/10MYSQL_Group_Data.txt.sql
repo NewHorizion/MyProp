d933520844e1b7d6710bdb6ac5b224dbdef2b287
Upendra Kumar <ambitious.eklavya@gmail.com>
2014-09-06 07:58:22 +0530

    script for group data

---
+++ dbScript/10MYSQL_Group_Data.txt.sql
@@ -0,0 +1,8 @@
+/*!40000 ALTER TABLE `groups` DISABLE KEYS */;
+INSERT INTO `groups` (`id`,`group_name`) VALUES 
+ (1,'admin'),
+ (2,'owner'),
+ (3,'agent'),
+ (4,'builder'),
+ (5,'reviewer');
+/*!40000 ALTER TABLE `groups` ENABLE KEYS */;
