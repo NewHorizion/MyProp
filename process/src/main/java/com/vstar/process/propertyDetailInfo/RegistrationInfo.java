67422bcddb0c0be064e63ac3f0f69e935ed48cc0
Vivek Chauhan <vivekchauhan.it@gmail.com>
2014-09-12 01:53:47 +0530

    Registration Info bean.

---
+++ process/src/main/java/com/vstar/process/propertyDetailInfo/RegistrationInfo.java
@@ -0,0 +1,87 @@
+package com.vstar.process.propertyDetailInfo;
+
+/**
+ * Registration Info bean
+ *
+ */
+public class RegistrationInfo
+{
+  private String userType;
+  private String userName;
+  private String mobileNumber;
+  private int cityId;
+  private String password;
+  private String emailId;
+  private String landlineNumber;
+
+  public String getUserType()
+  {
+    return userType;
+  }
+
+  public void setUserType(String userType)
+  {
+    this.userType = userType;
+  }
+
+  public String getUserName()
+  {
+    return userName;
+  }
+
+  public void setUserName(String userName)
+  {
+    this.userName = userName;
+  }
+
+  public String getMobileNumber()
+  {
+    return mobileNumber;
+  }
+
+  public void setMobileNumber(String mobileNumber)
+  {
+    this.mobileNumber = mobileNumber;
+  }
+
+  public int getCityId()
+  {
+    return cityId;
+  }
+
+  public void setCityId(int cityId)
+  {
+    this.cityId = cityId;
+  }
+
+  public String getPassword()
+  {
+    return password;
+  }
+
+  public void setPassword(String password)
+  {
+    this.password = password;
+  }
+
+  public String getEmailId()
+  {
+    return emailId;
+  }
+
+  public void setEmailId(String emailId)
+  {
+    this.emailId = emailId;
+  }
+
+  public String getLandlineNumber()
+  {
+    return landlineNumber;
+  }
+
+  public void setLandlineNumber(String landlineNumber)
+  {
+    this.landlineNumber = landlineNumber;
+  }
+
+}
