85cc8df8d1da8ac7d267a593db4ba50ee4b0229c
Vivek Chauhan <vivekchauhan.it@gmail.com>
2014-10-05 16:39:30 +0530

    MyConpany link setting.

--- war/src/main/webapp/js/scripts/mainScript.js
+++ war/src/main/webapp/js/scripts/mainScript.js
@@ -167,6 +167,7 @@ scotchApp
 // create the controller and inject Angular's $scope
 scotchApp.controller('mainController', function($scope, $rootScope) {
 	// create a message to display in our view
+	$rootScope.userType = '';
 	$scope.message = 'Everyone come and see how good I look!';
 });
 
